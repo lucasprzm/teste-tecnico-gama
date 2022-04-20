efff3675125c8d291beed1ce6e9343a5b968c484
Lucas Prazeres de Matos <lucasprzm@gmail.com>
2022-04-20 12:33:20 -0300

    alteração rota users para produtos

--- back-desafio-xp/app.js
+++ back-desafio-xp/app.js
@@ -5,7 +5,7 @@ var cookieParser = require("cookie-parser");
 var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
+var produtosRouter = require("./routes/produtos");
 
 var app = express();
 const db = require("./database");
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/produtos", produtosRouter);
 db.hasConnection();
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
